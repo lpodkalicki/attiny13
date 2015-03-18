f5f7866cba8389a4cac7c67ffeb4a7bfeb1025da
Łukasz Marcin Podkalicki <l.podkalicki@gmail.com>
2015-03-18 18:15:21 +0100

    A little improvement for first sketch

--- blink/main.c
+++ blink/main.c
@@ -1,25 +1,25 @@
 /*
  * Copyright (c) 2015 by Łukasz Marcin Podkalicki <lukasz@podkalicki.com>
- * ATtiny13 - blink
+ * ATtiny13 - blinking led
  */
+
+#define F_CPU	(1000000UL)
 
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define	sleep(x) _delay_ms(x)
-
 int
 main(void)
 {
 
-    DDRB = 1 << DDB4; 		// Set up Port B pin 4 mode to output
-    PORTB = 0; 			// Set up Port B data to be all low
+	DDRB = 1 << DDB4; 		// Set up Port B pin 4 mode to output
+	PORTB = 0; 			// Set up Port B data to be all low
 
-    while (1) {
-        PORTB ^= 1 << PB4; 	// Toggle Port B pin 4 output state
-        sleep(100); 		// Pause for a 100ms
-    }
+	while (1) {
+		PORTB ^= 1 << PB4; 	// Toggle Port B pin 4 output state
+		_delay_ms(200); 	// Pause for a 200ms
+	}
 
-    return (0);
+	return (0);
 }
